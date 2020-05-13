eb0e273268602c25eee51f4fcf352a2fbc4bdcbd
Ruslan Zanoch <ruslan@ucoz-team.net>
2020-05-13 11:24:06 +0300

    a bit prettier output at the /process

--- scripts/process.py
+++ scripts/process.py
@@ -8,19 +8,23 @@ def find_process_by_name(pattern):
     ls = []
     psutil.PROCFS_PATH = "/host/proc"
     for p in psutil.process_iter():
-        name_ = ""
+        name_, username_ = "", ""
+        pid_ = 0
         try:
             name_ = p.name()
+            pid_ = p.pid
+            username_ = p.username()
         except (psutil.AccessDenied, psutil.ZombieProcess):
             pass
         except psutil.NoSuchProcess:
             continue
         if name_.startswith(pattern):
-            ls.append(name_)
+            ls.append({'name': name_, 'user': username_, 'pid': str(pid_)})
     return ls
 
 
 result = find_process_by_name(sys.argv[1])
 exitCode = 0 if result.__len__() > 0 else 1
+
 print(result)
 exit(exitCode)
